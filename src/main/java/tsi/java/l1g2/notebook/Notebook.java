a036d2cfec71244a1ab1d1abaac4690e93c7e34c
AF <ariadna_f@inbox.lv>
2017-09-19 20:51:17 +0300

    add extends

--- src/main/java/tsi/java/l1g2/notebook/Notebook.java
+++ src/main/java/tsi/java/l1g2/notebook/Notebook.java
@@ -9,15 +9,27 @@ public class Notebook {
     private final List<Record> records = new ArrayList<>();
 
     @Command
-    public void create(String firstName, String lastName, String email, String... phones) {
-        Record r = new Record();
+    public void createPerson(String firstName, String lastName, String email, String... phones) {
+        Person r = new Person();
         r.setFirstName(firstName);
         r.setLastName(lastName);
         r.addPhones(phones);
         r.setEmail(email);
         records.add(r);
     }
-
+    @Command
+    public void createNote(String text) {
+        Note rec = new Note();
+        rec.setText(text);
+        records.add(rec);
+    }
+    @Command
+    public void createReminder(String text, String time) {
+        Reminder rec = new Reminder();
+        rec.setText(text);
+        rec.setTime(time);
+        records.add(rec);
+    }
     @Command
     public List<Record> list() {
         return records;
--- src/main/java/tsi/java/l1g2/notebook/Record.java
+++ src/main/java/tsi/java/l1g2/notebook/Record.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Record {
+public abstract class Record {
     private static int counter = 0;
     private int id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private List<String> phones = new ArrayList<>();
+
 
     public Record() {
         counter++;
@@ -21,43 +18,5 @@ public class Record {
         return id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() { return email; }
-
-    public void setEmail(String email) { this.email = email; }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public List<String> getPhones() {
-        return phones;
-    }
-
-    public void addPhones(String... phones) {
-      //  this.phone = phone;
-        Collections.addAll(this.phones, phones);
-    }
-
-    @Override
-    public String toString() {
-        return "Record{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", e-mail='" + email + '\'' +
-                ", phones='" + phones + '\'' +
-                '}';
-    }
 }
